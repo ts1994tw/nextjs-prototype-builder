ad0a7bbc4ebbf28adebdd465a90708b22c250c96
ts1994tw <rocky61614@yahoo.com.tw>
2023-05-25 22:11:21 +0800

    fix missing storybook staticDirs config

---
+++ .storybook/main.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  features: {
+    storyStoreV7: true
+  },
+  staticDirs: ['../public'],
+  core: {
+    builder: {
+      name: 'webpack5',
+      options: {
+        lazyCompilation: true,
+        fsCache: true
+      }
+    }
+  },
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-dark-mode', {
+    name: '@storybook/addon-postcss',
+    options: {
+      postcssLoaderOptions: {
+        implementation: require('postcss')
+      }
+    }
+  }],
+  webpackFinal: async (config, {
+    configType
+  }) => {
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules'];
+    config.resolve.alias = { ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src/')
+    }; // add SCSS support for CSS Modules
+    // https://github.com/storybookjs/storybook/issues/12464#issuecomment-824107014
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader?modules&importLoaders', 'sass-loader'],
+      include: path.resolve(__dirname, '../')
+    });
+    return config;
+  },
+  framework: '@storybook/react'
+};
